d518302ae27b45fe9193155f648cc7b5f384ec86
Mohammad Haidar <mhd4.hz@gmail.com>
2023-03-20 16:41:35 +0300

    DataContext1

---
+++ BarberShop/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using BarberShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberShop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions <DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Barber> Barbers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<BarberEmployee> BarberEmployees { get; set; }
+    }
+}
