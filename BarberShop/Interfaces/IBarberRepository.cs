eb747dd1d76b2d3a21b2fd4ca0757dda9c2d978a
Mohammad Haidar <mhd4.hz@gmail.com>
2023-03-23 12:40:07 +0300

    interface barber half ready

---
+++ BarberShop/Interfaces/IBarberRepository.cs
@@ -0,0 +1,18 @@
+﻿using BarberShop.Models;
+
+namespace BarberShop.Interfaces
+{
+    public interface IBarberRepository
+    {
+        ICollection<Barber> GetBarbers();
+        Barber GetBarber(int id);
+        Barber GetBarberByReservation(int reservationId);
+        bool BarberExists(int id);
+        bool CeateBarber(Barber barber);
+        bool DeleteBarber(int id);
+        bool UpdateBarber(Barber barber);
+        ICollection<BarberEmployee> GetEmployeesOfBarber(int id);
+        ICollection<BarberService> GetBarberServices(int id);
+        bool Save();
+    }
+}
---
+++ BarberShop/Repository/BarberRepository.cs
@@ -0,0 +1,17 @@
+﻿using BarberShop.Data;
+using BarberShop.Interfaces;
+
+namespace BarberShop.Repository
+{
+    public class BarberRepository : IBarberRepository
+    {
+        private readonly DataContext _context;
+
+        public BarberRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
